eb3bdf94291f50d73073be1870bcae6b1214b7fc
Gaurav Chauhan <53932940+gaurav-zwt@users.noreply.github.com>
2023-10-09 19:17:52 +0530

    Some Changes

--- edit.js
+++ edit.js
@@ -6,10 +6,9 @@
 import { __ } from '@wordpress/i18n';
 import { RadioControl, Button } from '@wordpress/components';
 import { registerBlockType } from '@wordpress/blocks';
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 import { MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
 import { useState } from '@wordpress/element';
-
+import './recent_post.js';
 
 
 /**
@@ -113,73 +112,7 @@ export default function Edit({ attributes, setAttributes }) {
 				      </div>
 	            	: 
 	            		selectedOption == 'post-gallery'
-	            		? <div { ...useBlockProps() }  className={ 'alignwide' }>
-				            <InnerBlocks
-								template={[
-									['core/query', {
-											queryId:44,
-											query: {
-												offset:0,
-												postType: 'post',
-												order: 'desc',
-												orderBy: 'date',
-												perPage: 6,
-												author: '',
-												search: '',
-												sticky:'',
-											},
-											displayLayout: { type: 'flex', columns: 3 },
-											align: 'wide'							
-										},
-										[
-											['core/post-template', { align: 'wide' },
-												[
-													['core/post-featured-image', { isLink: 1 }],
-													['core/group', {											
-															style: {
-																color:{background: '#f7fbfb'},												
-																border: { radius: { bottomLeft: '15px', bottomRight: '15px' } },
-																spacing: { padding: { bottom: '25px', top: '25px', right: '25px', left: '25px' } },
-															}
-														},
-														[
-															['core/post-title', {
-																isLink: 1,
-																style: {
-																	typography: { fontSize: '26px', fontWeight: 700 },
-																}
-															}],
-															
-															['core/post-excerpt', {
-																textAlign: 'left',
-																moreText: 'Learn More',
-																className: 'blog_post_learn_more',
-																style: {
-																	typography: { fontSize: '16px'},
-																}
-															}],
-														]
-													]
-												]
-											],
-											['core/separator', { className: 'alignwide is-style-wide' }],
-											['core/query-pagination', {
-													paginationArrow: 'arrow',
-													align: 'wide',
-													layout: {type:'flex', justifyContent:'space-between'},
-												},
-												[
-													['core/query-pagination-previous', {}],
-													['core/query-pagination-numbers', {}],
-													['core/query-pagination-next', {}],
-												],
-											],
-										]
-									]
-				                ] }
-				                templateLock="all"
-				            />
-				        </div>
+	            		? <recent_post />
 	            		: 'Please Select Option'
 	            	
 	        }
