a2ac39ced1edaf1cf015a794e1c05e5b057d59dd
Maitri Prajapti <maitri@zealousweb.com>
2024-07-18 15:23:12 +0530

    applying css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '2d4a5f53524ca3c8aa3e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '12624f269eb536e562cc');
