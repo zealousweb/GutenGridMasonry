b135b6c310964712e60589dbb7780618390dc7a8
Maitri Prajapti <maitri@zealousweb.com>
2024-07-24 11:33:18 +0530

    update build file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '7d5c5e48f2fe0080e57e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f671cdfe45fcfb9f2c28');
