7a8d4cc5ce9cca37f7e8236edc014fb7b21ba1b5
Maitri Prajapti <maitri@zealousweb.com>
2024-07-24 19:01:58 +0530

    Clickable Full Post

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'ed0a02ac172ad7418202');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f99ee1953cd92468c3f8');
