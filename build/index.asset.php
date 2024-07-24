d16669317bd8d67023121844cf8147d23293864f
Maitri Prajapti <maitri@zealousweb.com>
2024-07-24 19:36:14 +0530

    add the border part

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f99ee1953cd92468c3f8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '52742ba23e2ba2689960');
