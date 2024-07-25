3e67616db67b46b00e4a90eb4bfadf9a191b10ba
Maitri Prajapti <maitri@zealousweb.com>
2024-07-25 11:04:58 +0530

    caption part

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '52742ba23e2ba2689960');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f3400be327e353177627');
